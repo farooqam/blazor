d1bcb76d3cfab99b9adce2889b17041a7f0d4bf6
Farooq Mahmud <farooq_am@hotmail.com>
2019-07-06 00:13:24 -0700

    Put Calculator logic in code behind file.

---
+++ Calculator/Pages/CalculatorComponent.razor.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.AspNetCore.Components;
+
+namespace Calculator.Pages
+{
+    public class CalculatorComponentModel : ComponentBase
+    {
+        public double Operand1 { get; set; }
+        public double Operand2 { get; set; }
+        public double Result { get; set; }
+
+        public void Add()
+        {
+            Result = Operand1 + Operand2;
+        }
+
+        public void Subtract()
+        {
+            Result = Operand1 - Operand2;
+        }
+
+        public void Multiply()
+        {
+            Result = Operand1 * Operand2;
+        }
+
+        public void Divide()
+        {
+            if (Operand2 == 0)
+            {
+                throw new Exception("Cannot divide by zero.");
+            }
+
+            Result = Operand1 / Operand2;
+        }
+    }
+}
