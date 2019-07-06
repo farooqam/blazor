ea340b524facdb66ab7e0f53a092851c772e996b
farooqam <farooq_am@hotmail.com>
2019-07-06 00:50:01 -0700

    Calculator (#2)

---
+++ Calculator/Pages/CalculatorComponent.razor.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Components;
+using Calculator.Shared;
+
+namespace Calculator.Pages
+{
+    public class CalculatorComponentModel : ComponentBase
+    {
+        [Inject]
+        protected ToastService ToastService { get; set; }
+        protected double Operand1 { get; set; }
+        protected double Operand2 { get; set; }
+        protected double Result { get; set; }
+
+        protected void Add()
+        {
+            Result = Operand1 + Operand2;
+        }
+
+        protected void Subtract()
+        {
+            Result = Operand1 - Operand2;
+        }
+
+        protected void Multiply()
+        {
+            Result = Operand1 * Operand2;
+        }
+
+        protected void Divide()
+        {
+            if (Operand2 == 0)
+            {
+                ToastService.ShowToast("Cannot divide by zero.");
+                return;
+            }
+
+            Result = Operand1 / Operand2;
+        }
+    }
+}
